f80c2aff14ee5b4d7c37599288d06c7300cd7707
root <root@debian.home>
2011-11-18 09:43:38 +0100

    Modif : bloquage des ports et ping

--- fwkm.c
+++ fwkm.c
@@ -1,22 +1,24 @@
 #define __KERNEL__
 #define MODULE
 
-#include <linux/module.h>	//MODULE_LICENCE, ...
+#include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/netfilter_ipv4.h>	// fonctions netfilter
+#include <linux/netfilter_ipv4.h>
 
-#include <linux/ip.h>		// structure iphdr
-#include <linux/tcp.h>		// structure tcphdr
+#include <linux/ip.h>	
+#include <linux/tcp.h>	
 #include <linux/udp.h>
 #include <linux/icmp.h>
 
-MODULE_LICENSE("GPL");		// informations sur le module
-MODULE_AUTHOR("Julien STERCKEMAN");
-MODULE_DESCRIPTION("Mini firewall");
+MODULE_LICENSE("GPL");		
+MODULE_AUTHOR("Si ça marche : Vincent Maël - Millet Gaël, Si ça plante : Hugo Viricel");
+MODULE_DESCRIPTION("Firewall-trop-bien");
 
-struct nf_hook_ops input_filter;	// caracteristiques du hook
+struct nf_hook_ops input_filter;	
+int ports_udp[3] = {80,21,69};
+int ports_tcp[3] = {80,21,69};
 
-// fonction qui va etre appelee pour le hook
+
 unsigned int input_hook(unsigned int hooknum, struct sk_buff *skb,
 			const struct net_device *in,
 			const struct net_device *out,
@@ -27,75 +29,101 @@ unsigned int input_hook(unsigned int hooknum, struct sk_buff *skb,
 	struct udphdr *udph;
 	struct icmphdr *icmph;
 
-	//iph = (*skb)->nh.iph;	// skb est une structure contenant le paquet
+	//iph = (*skb)->nh.iph;	
 	iph = (struct iphdr *)skb_network_header(skb);
-				// cf /linux/sk_buff.h
+	
 	
 	if(iph->protocol == 1)	// ICMP
 	{
-		// on prend l'en-tête icmp
 		icmph = (struct icmphdr *)skb_transport_header(skb);
 		
-		printk("<1>firewall : aime pas les ping de 0x%x!!\n", iph->saddr);
+		printk("<1>Firewall : A bloqué un ping les pings de 0x%s !\n", iph->saddr);
 		
-		return NF_DROP;	// on ignore tous les paquets icmp
-		// ceci est juste pour l'exemple, en réalité il ne
-		// faudrait refuser que les ICMP_ECHO et pas tous,
-		// sinon nous n'orions plus les erreurs Host Unreacheable...
+		return NF_DROP;	
 	}
-	/*else if(iph->protocol == 6)	// TCP
+	else if(iph->protocol == 6)	// TCP
 	{
 		tcph = (struct tcphdr *)skb_transport_header(skb);
 	
-		printk("<1>firewall : paquet tcp ports=%d portd=%d\n", 
-				tcph->source, tcph->dest);
+		
+		int posTCPsource = -1;
+		int posTCPdest = -1;
+		int i;
+		int j;
 
-		// c'est ici que l'on vérifie les ports, les flags, ...
-		// et que l'on décide si l'on accepte le paquet
-		return NF_DROP;
+		if (69==tcph->source)
+		   		posTCPsource=1;
+
+		for (i=0 ; (i<3)&&(posTCPsource==-1) ; i++)
+	       		if (ports_tcp[i]==(int)tcph->source)
+		   		posTCPsource=i;
+
+		for (j=0 ; (j<3)&&(posTCPdest==-1) ; j++)
+	       		if (ports_tcp[j]==(int)tcph->dest)
+		   		posTCPdest=j;
+
+		if((posTCPsource!=-1)||(posTCPdest!=-1)){
+			printk("<1>Firewall : Bloque port TCP source %d!!\n", tcph->source);
+			printk("<1>Firewall : Bloque port TCP dest %d!!\n", tcph->dest);
+
+			return NF_DROP;
+		}
 	}
-	else if(iph->protocol == 17)	// UDP
+	else if(iph->protocol == 17) // UDP
 	{
 		udph = (struct udphdr *)skb_transport_header(skb);
 		
-		printk("<1>firewall : paquet udp ports=%d portd=%d\n",
-				udph->source, udph->dest);
 
-		// pareil que pour tcp
-	}*/
-	
-	// si ca passe les tests on accepte
+		int posUDPsource = -1;
+		int posUDPdest = -1;
+		int i;
+		int j;
+
+		for (i=0 ; (i<3)&&(posUDPsource==-1) ; i++)
+	       		if (ports_udp[i]==(int)udph->source)
+		   		posUDPsource=i;
+
+		for (j=0 ; (j<3)&&(posUDPdest==-1) ; j++)
+	       		if (ports_udp[j]==(int)udph->dest)
+		   		posUDPdest=j;
+
+		if((posUDPsource!=-1)||(posUDPdest!=-1)){
+			printk("<1>Firewall : Bloque port UDP source %d!!\n", udph->source);
+			printk("<1>Firewall : Bloque port UDP dest %d!!\n", udph->dest);
+
+			return NF_DROP;
+		}
+	}
+
 	return NF_ACCEPT;
 }
 
-int init_module(void)	// fonction "main()" des modules
+int init_module(void)
 {
 	int result;
 
-	// on indique les caracteristiques du hook d'entree
-	input_filter.list.next = NULL;	// le kernel va les remplir
+	
+	input_filter.list.next = NULL;
 	input_filter.list.prev = NULL;
 	
-	input_filter.hook = (nf_hookfn *)input_hook;	// fonction hook
-	input_filter.pf = PF_INET;			// IPv4
-	input_filter.hooknum = NF_INET_LOCAL_IN;		// type de hook
+	input_filter.hook = (nf_hookfn *)input_hook;	
+	input_filter.pf = PF_INET;			
+	input_filter.hooknum = NF_INET_LOCAL_IN;
 
-	// on peut faire aussi des hook NF_IP_LOCAL_OUT pour
-	// empecher des troyens ou spywares et autres
-	// et NF_IP_FORWARD pour filtrer au niveau de la passerelle
+	
 
-	result = nf_register_hook(&input_filter);	// retourne 0 si ok
+	result = nf_register_hook(&input_filter);
 	if(result)
 	{
-		printk("<1>firewall : erreur nf_register_hook !!!\n");
+		printk("<1>Firewall : Erreur nf_register_hook !\n");
 		return 1;
 	}
-	printk("<1>firewall : module charge.\n");
+	printk("<1>Firewall : Chargement OK !\n");
 	return 0;
 }
 
-void cleanup_module(void)	// appelee par rmmod
+void cleanup_module(void)
 {
 	nf_unregister_hook(&input_filter);
-	printk("<1>firewall : module decharge.\n");
+	printk("<1>Firewall : Programme déchargé.\n");
 }
