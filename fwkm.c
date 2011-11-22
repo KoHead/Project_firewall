a617cf21d0a88041b0e2340b6d4740ee24467b40
Gael Millet <millet.gael@gmail.com>
2011-11-22 16:54:20 +0100

    Code de gael trie avec le tableau des protocoles

--- fwkm.c
+++ fwkm.c
@@ -4,7 +4,6 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/netfilter_ipv4.h>
-
 #include <linux/ip.h>	
 #include <linux/tcp.h>	
 #include <linux/udp.h>
@@ -18,90 +17,90 @@ struct nf_hook_ops input_filter;
 int ports_udp[3] = {80,21,69};
 int ports_tcp[3] = {80,21,69};
 
+char* protocoles[134] = {"HOPOPT","ICMP","IGMP","GGP","IP","ST","TCP","CBT","EGP","IGP","BBN-RCC-MON","NVP-II","PUP","ARGUS","EMCON","XNET","CHAOS","UDP","MUX","DCN-MEAS ","HMP","PRM","XNS-IDP","TRUNK-1","TRUNK-2","LEAF-1","LEAF-2","RDP","IRTP","ISO-TP4","NETBLT","MFE-NSP","MERIT-INP","SEP","3PC","IDPR","XTP","DDP","IDPR-CMTP","TP++","IL","IPv6","SDRP","IPv6-Route","IPv6-Route","IDRP","RSVP","GRE","MHRP","BNA","ESP","AH","I-NLSP","SWIPE","NARP","MOBILE","TLSP","SKIP","IPv6-ICMP","IPv6-NoNxt","IPv6-Opts","61","CFTP","63","SAT-EXPAK","KRYPTOLAN","RVD","IPPC","68","SAT-MON","VISA","IPCV","CPNX","CPHB","WSN","PVP","BR-SAT-MON","SUN-ND","WB-MON","WB-EXPAK","ISO-IP","VMTP","SECURE-VMTP","VINES","TTP","NSFNET-IGP","DGP","TCF","EIGRP","OSPFIGP","Sprite-RPC","LARP","MTP ","AX.25","IPIP","MICP","SCC-SP","ETHERIP","ENCAP","99","GMTP","IFMP","PNNI","PIM","ARIS","SCPS","QNX","A/N","IPComp","SNP","Compaq-Peer","IPX-in-IP","VRRP","PGM","114","L2TP","DDX","IATP","STP","SRP","UTI","SMP","SM","PTP","ISIS","FIRE ","CRTP","CRUDP","SSCOPMCE ","IPLT ","SPS","PIPE","SCTP","FC"}; // Le tableau est indexé par n° de protocole, donc par exemple le protocole n°6 correspond bien à TCP
+
+struct Option {
+	int Protocol;
+	int Port; //TODO : Affectation d'un nom au lieu d'un n° de protocole
+	int InOut; // 0 = Out - 1 = IN
+	int Access; // 0 = DENY - 1 = ALLOW
+	//TODO : gestion des IP
+};
+struct Option OptionsArray[2];
 
 unsigned int input_hook(unsigned int hooknum, struct sk_buff *skb,
 			const struct net_device *in,
 			const struct net_device *out,
 			int (*okfn)(struct sk_buff *))
 {
-	struct iphdr *iph;
-	struct tcphdr *tcph;
-	struct udphdr *udph;
-	struct icmphdr *icmph;
-
-	//iph = (*skb)->nh.iph;	
+	
+	int i;	
+	int drop = 0;
+	struct iphdr *iph;	
 	iph = (struct iphdr *)skb_network_header(skb);
-	
-	
-	if(iph->protocol == 1)	// ICMP
+	for (i=0 ; i<2 ; i++)
 	{
-		icmph = (struct icmphdr *)skb_transport_header(skb);
-		
-		printk("<1>Firewall : A bloqué un ping les pings de 0x%s !\n", iph->saddr);
-		
-		return NF_DROP;	
-	}
-	else if(iph->protocol == 6)	// TCP
-	{
-		tcph = (struct tcphdr *)skb_transport_header(skb);
-	
-		
-		int posTCPsource = -1;
-		int posTCPdest = -1;
-		int i;
-		int j;
-
-		if (69==tcph->source)
-		   		posTCPsource=1;
-
-		for (i=0 ; (i<3)&&(posTCPsource==-1) ; i++)
-	       		if (ports_tcp[i]==(int)tcph->source)
-		   		posTCPsource=i;
-
-		for (j=0 ; (j<3)&&(posTCPdest==-1) ; j++)
-	       		if (ports_tcp[j]==(int)tcph->dest)
-		   		posTCPdest=j;
-
-		if((posTCPsource!=-1)||(posTCPdest!=-1)){
-			printk("<1>Firewall : Bloque port TCP source %d!!\n", tcph->source);
-			printk("<1>Firewall : Bloque port TCP dest %d!!\n", tcph->dest);
-
-			return NF_DROP;
+		if (OptionsArray[i].Protocol == iph->protocol)//TODO : bloquer tous les protocoles ?
+		{
+			if((iph->protocol == 6)||(iph->protocol == 17))
+			{
+				
+				if(iph->protocol==6)
+				{
+					struct tcphdr *protoh;					
+					protoh = (struct tcphdr *)skb_transport_header(skb);
+					if((OptionsArray[i].Port==(int)protoh->source)||(OptionsArray[i].Port==(int)protoh->dest))
+					{
+						drop = 1;
+						printk("<1>Firewall : A bloqué une ouverture de port !\n");
+					}
+				}
+				else
+				{
+					struct udphdr *protoh;	
+					protoh = (struct udphdr *)skb_transport_header(skb);
+					if((OptionsArray[i].Port==(int)protoh->source)||(OptionsArray[i].Port==(int)protoh->dest))//TODO : Gestion IN/OUT distincte
+					{
+						drop = 1;
+						printk("<1>Firewall : A bloqué une ouverture de port !\n");
+					}
+				}
+				//TODO : Fontion pour tous les ports ?
+				
+			}
+			else{
+				drop = 1;
+				printk("<1>Firewall : A bloqué un protocole !\n");
+			}
+			//TODO : Gestion des ALLOW/DENY ?
 		}
 	}
-	else if(iph->protocol == 17) // UDP
-	{
-		udph = (struct udphdr *)skb_transport_header(skb);
-		
-
-		int posUDPsource = -1;
-		int posUDPdest = -1;
-		int i;
-		int j;
-
-		for (i=0 ; (i<3)&&(posUDPsource==-1) ; i++)
-	       		if (ports_udp[i]==(int)udph->source)
-		   		posUDPsource=i;
-
-		for (j=0 ; (j<3)&&(posUDPdest==-1) ; j++)
-	       		if (ports_udp[j]==(int)udph->dest)
-		   		posUDPdest=j;
-
-		if((posUDPsource!=-1)||(posUDPdest!=-1)){
-			printk("<1>Firewall : Bloque port UDP source %d!!\n", udph->source);
-			printk("<1>Firewall : Bloque port UDP dest %d!!\n", udph->dest);
-
-			return NF_DROP;
-		}
+	if(drop==1){
+		return NF_DROP;
 	}
-
-	return NF_ACCEPT;
+	else{
+		return NF_ACCEPT;
+	}
 }
 
 int init_module(void)
 {
 	int result;
 
+
+	struct Option myOption;
+
+	myOption.Protocol = 6;
+	myOption.Port = 80;
+	myOption.InOut = 0;
+	myOption.Access = 0;
+	OptionsArray[0] = myOption;
+	myOption.Protocol = 1;
+	myOption.Port = 20;
+	myOption.InOut = 0;
+	myOption.Access = 0;
+	OptionsArray[1] = myOption;
+	//TODO : chargement de OptionsArray depuis un fichier
 	
 	input_filter.list.next = NULL;
 	input_filter.list.prev = NULL;
