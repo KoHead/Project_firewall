1e4e87339d31477cee504449384d8cd5e656018e
Gael Millet <millet.gael@gmail.com>
2011-11-24 00:38:43 +0100

    Gaël : Ajout des plages de ports

--- fwkm.c
+++ fwkm.c
@@ -17,11 +17,22 @@ struct nf_hook_ops input_filter;
 int ports_udp[3] = {80,21,69};
 int ports_tcp[3] = {80,21,69};
 
-char* protocoles[134] = {"HOPOPT","ICMP","IGMP","GGP","IP","ST","TCP","CBT","EGP","IGP","BBN-RCC-MON","NVP-II","PUP","ARGUS","EMCON","XNET","CHAOS","UDP","MUX","DCN-MEAS ","HMP","PRM","XNS-IDP","TRUNK-1","TRUNK-2","LEAF-1","LEAF-2","RDP","IRTP","ISO-TP4","NETBLT","MFE-NSP","MERIT-INP","SEP","3PC","IDPR","XTP","DDP","IDPR-CMTP","TP++","IL","IPv6","SDRP","IPv6-Route","IPv6-Route","IDRP","RSVP","GRE","MHRP","BNA","ESP","AH","I-NLSP","SWIPE","NARP","MOBILE","TLSP","SKIP","IPv6-ICMP","IPv6-NoNxt","IPv6-Opts","61","CFTP","63","SAT-EXPAK","KRYPTOLAN","RVD","IPPC","68","SAT-MON","VISA","IPCV","CPNX","CPHB","WSN","PVP","BR-SAT-MON","SUN-ND","WB-MON","WB-EXPAK","ISO-IP","VMTP","SECURE-VMTP","VINES","TTP","NSFNET-IGP","DGP","TCF","EIGRP","OSPFIGP","Sprite-RPC","LARP","MTP ","AX.25","IPIP","MICP","SCC-SP","ETHERIP","ENCAP","99","GMTP","IFMP","PNNI","PIM","ARIS","SCPS","QNX","A/N","IPComp","SNP","Compaq-Peer","IPX-in-IP","VRRP","PGM","114","L2TP","DDX","IATP","STP","SRP","UTI","SMP","SM","PTP","ISIS","FIRE ","CRTP","CRUDP","SSCOPMCE ","IPLT ","SPS","PIPE","SCTP","FC"}; // Le tableau est indexé par n° de protocole, donc par exemple le protocole n°6 correspond bien à TCP
+char* protocoles[134] = {"HOPOPT","ICMP","IGMP","GGP","IP","ST","TCP","CBT","EGP","IGP","BBN-RCC-MON","NVP-II","PUP","ARGUS",
+			"EMCON","XNET","CHAOS","UDP","MUX","DCN-MEAS ","HMP","PRM","XNS-IDP","TRUNK-1","TRUNK-2","LEAF-1",
+			"LEAF-2","RDP","IRTP","ISO-TP4","NETBLT","MFE-NSP","MERIT-INP","SEP","3PC","IDPR","XTP","DDP","IDPR-CMTP",
+			"TP++","IL","IPv6","SDRP","IPv6-Route","IPv6-Route","IDRP","RSVP","GRE","MHRP","BNA","ESP","AH","I-NLSP",
+			"SWIPE","NARP","MOBILE","TLSP","SKIP","IPv6-ICMP","IPv6-NoNxt","IPv6-Opts","61","CFTP","63","SAT-EXPAK",
+			"KRYPTOLAN","RVD","IPPC","68","SAT-MON","VISA","IPCV","CPNX","CPHB","WSN","PVP","BR-SAT-MON","SUN-ND",
+			"WB-MON","WB-EXPAK","ISO-IP","VMTP","SECURE-VMTP","VINES","TTP","NSFNET-IGP","DGP","TCF","EIGRP","OSPFIGP",
+			"Sprite-RPC","LARP","MTP ","AX.25","IPIP","MICP","SCC-SP","ETHERIP","ENCAP","99","GMTP","IFMP","PNNI","PIM",
+			"ARIS","SCPS","QNX","A/N","IPComp","SNP","Compaq-Peer","IPX-in-IP","VRRP","PGM","114","L2TP","DDX","IATP",
+			"STP","SRP","UTI","SMP","SM","PTP","ISIS","FIRE ","CRTP","CRUDP","SSCOPMCE ","IPLT ","SPS","PIPE","SCTP","FC"};
+			 // Le tableau est indexé par n° de protocole, donc par exemple le protocole n°6 correspond bien à TCP
 
 struct Option {
 	int Protocol;
-	int Port; //TODO : Affectation d'un nom au lieu d'un n° de protocole
+	int MinPort;
+	int MaxPort;
 	int InOut; // 0 = Out - 1 = IN
 	int Access; // 0 = DENY - 1 = ALLOW
 	//TODO : gestion des IP
@@ -32,9 +43,8 @@ unsigned int input_hook(unsigned int hooknum, struct sk_buff *skb,
 			const struct net_device *in,
 			const struct net_device *out,
 			int (*okfn)(struct sk_buff *))
-{
-	
-	int i;	
+{	
+	int i, interval = 0;	
 	int drop = 0;
 	struct iphdr *iph;	
 	iph = (struct iphdr *)skb_network_header(skb);
@@ -42,33 +52,62 @@ unsigned int input_hook(unsigned int hooknum, struct sk_buff *skb,
 	{
 		if (OptionsArray[i].Protocol == iph->protocol)//TODO : bloquer tous les protocoles ?
 		{
-			if((iph->protocol == 6)||(iph->protocol == 17))
+			if((iph->protocol == 6)||(iph->protocol == 17)) // Si TCP/UDP (donc avec ports)
 			{
-				
-				if(iph->protocol==6)
-				{
-					struct tcphdr *protoh;					
-					protoh = (struct tcphdr *)skb_transport_header(skb);
-					if((OptionsArray[i].Port==(int)protoh->source)||(OptionsArray[i].Port==(int)protoh->dest))
+				if(OptionsArray[i].MinPort != OptionsArray[i].MaxPort) // Si plage de port
+				{					
+					if(iph->protocol==6)
+					{											
+						struct tcphdr *protoh;					
+						protoh = (struct tcphdr *)skb_transport_header(skb);
+						if(((OptionsArray[i].MinPort<=protoh->source)&&(OptionsArray[i].MaxPort>=protoh->source)&&(OptionsArray[i].InOut==0)) ||
+						   ((OptionsArray[i].MinPort<=protoh->dest)&&(OptionsArray[i].MaxPort>=protoh->dest)&&(OptionsArray[i].InOut==1)))
+						{
+							drop = 1;
+							printk("<1>Firewall : A bloqué une ouverture de port !\n");
+						}
+					}
+					else
 					{
-						drop = 1;
-						printk("<1>Firewall : A bloqué une ouverture de port !\n");
+						struct udphdr *protoh;	
+						protoh = (struct udphdr *)skb_transport_header(skb);
+						if(((OptionsArray[i].MinPort<=protoh->source)&&(OptionsArray[i].MaxPort>=protoh->source)&&(OptionsArray[i].InOut==0)) ||
+						   ((OptionsArray[i].MinPort<=protoh->dest)&&(OptionsArray[i].MaxPort>=protoh->dest)&&(OptionsArray[i].InOut==1)))
+						{
+							drop = 1;
+							printk("<1>Firewall : A bloqué une ouverture de port !\n");
+						}
 					}
 				}
-				else
+				else if((OptionsArray[i].MinPort == -1) && (OptionsArray[i].MaxPort == -1)){ // Si tous les ports
+					drop = 1;
+				}
+				else if(OptionsArray[i].MinPort == OptionsArray[i].MaxPort) // Si port unique
 				{
-					struct udphdr *protoh;	
-					protoh = (struct udphdr *)skb_transport_header(skb);
-					if((OptionsArray[i].Port==(int)protoh->source)||(OptionsArray[i].Port==(int)protoh->dest))//TODO : Gestion IN/OUT distincte
+					if(iph->protocol==6)
 					{
-						drop = 1;
-						printk("<1>Firewall : A bloqué une ouverture de port !\n");
+						struct tcphdr *protoh;					
+						protoh = (struct tcphdr *)skb_transport_header(skb);
+						if(((OptionsArray[i].MinPort==protoh->source)&&(OptionsArray[i].InOut==0)) || ((OptionsArray[i].MinPort==protoh->dest)&&(OptionsArray[i].InOut==1)))
+						{
+							drop = 1;
+							printk("<1>Firewall : A bloqué une ouverture de port !\n");
+						}
+					}
+					else
+					{
+						struct udphdr *protoh;	
+						protoh = (struct udphdr *)skb_transport_header(skb);
+						if(((OptionsArray[i].MinPort==protoh->source)&&(OptionsArray[i].InOut==0)) || ((OptionsArray[i].MinPort==protoh->dest)&&(OptionsArray[i].InOut==1)))
+						{
+							drop = 1;
+							printk("<1>Firewall : A bloqué une ouverture de port !\n");
+						}
 					}
 				}
-				//TODO : Fontion pour tous les ports ?
 				
 			}
-			else{
+			else{ // Si autre protocole de TCP/UDP
 				drop = 1;
 				printk("<1>Firewall : A bloqué un protocole !\n");
 			}
@@ -91,13 +130,15 @@ int init_module(void)
 	struct Option myOption;
 
 	myOption.Protocol = 6;
-	myOption.Port = 80;
-	myOption.InOut = 0;
+	myOption.MinPort = 0;
+	myOption.MaxPort = 12000;
+	myOption.InOut = 1;
 	myOption.Access = 0;
 	OptionsArray[0] = myOption;
 	myOption.Protocol = 1;
-	myOption.Port = 20;
-	myOption.InOut = 0;
+	myOption.MinPort = -1;
+	myOption.MaxPort = -1;
+	myOption.InOut = 1;
 	myOption.Access = 0;
 	OptionsArray[1] = myOption;
 	//TODO : chargement de OptionsArray depuis un fichier
